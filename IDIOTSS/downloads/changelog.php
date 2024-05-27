ab990af31d9391176078b398bf81520dedbb0c25
banna12345bob <banna12345bob@outlook.com>
2024-05-27 20:30:13 +1000

    Updated mods

---
+++ IDIOTSS/downloads/changelog.php
@@ -0,0 +1,15 @@
+<html>
+    <?php
+        require_once("../scripts/createHeaders.php");
+        require_once("../scripts/createNav.php");
+        createHeader("Changelog", "List of changes");
+    ?>
+    <body>
+        <h1 style="color:hotpink">Changelog</h1>
+        <h4>V3.0.1</h4>
+        <ul>
+            <li>Updated Create: Numismatics from 1.0.4 to 1.0.6</li>
+        </ul>
+        <p><a href="<?php echo $path?>/downloads">Back</a></p>
+    </body>
+</html>
