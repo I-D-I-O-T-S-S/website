56e1a94dc03a5cdd85b61afae43ada9af8fc89d2
banna12345bob <banna12345bob@outlook.com>
2024-08-04 18:30:47 +1000

    starting work on making it look nice

--- IDIOTSS/index.php
+++ IDIOTSS/index.php
@@ -5,11 +5,13 @@
         require_once(__DIR__."/scripts/createNav.php")
     ?>
     <body>
+        <?php createNav() ?>
         <img src="<?php echo $path?>/images/egg.gif" style="display:inline-block" height=10%>
         <h1 style="display:inline-block; color:hotpink">Web design is my passion</h1>
         <img src="<?php echo $path?>/images/egg.gif" style="display:inline-block" height=10%>
         <p><a href="<?php echo $path?>/dynmap">Dynmap</a></p>
         <p><a href="<?php echo $path?>/train-map">Trains</a></p>
+        <h3><a href="<?php echo $path?>/downloads/IDIOTSS Launcher-setup.exe">Download Launcher<a></h3>
         <p><a href="<?php echo $path?>/downloads">Downloads</a></p>
     </body>
 </html>
--- IDIOTSS/scripts/createNav.php
+++ IDIOTSS/scripts/createNav.php
@@ -1,25 +1,28 @@
 <?php
 require_once(__DIR__."/createHeaders.php");
 ?>
+
+<?php function CreateNav() {
+    $path = "http://".$_SERVER['HTTP_HOST'];
+?>
 <!-- Imports the stylesheet -->
 <link rel="stylesheet" href="<?php echo $path?>/stylesheets/nav.css">
 <nav>
-    <!-- This is the links at the top of the page it is currently only the SCCC logo -->
-    <div class="links">
+    <div class="links" style="height: 200px;">
         <?php
     $options = array(
         // -------------------------- REALLY REALLY BAD WORKAROUND -------------------------- //
         // Can't figure out how impliment text so just make a 1200 x 630 image with the text you want
         array('link' => $path,       'image' => $path.'/favicon.png'),
-        array('link' => $path,       'image' => $path.'/favicon.png'),
-        array('link' => $path,       'image' => $path.'/favicon.png'),
-        array('link' => $path,       'image' => $path.'/favicon.png'),
+        // array('link' => $path,       'image' => $path.'/favicon.png'),
+        // array('link' => $path,       'image' => $path.'/favicon.png'),
+        // array('link' => $path,       'image' => $path.'/favicon.png'),
     );
     ?>
     <!-- I put the links as an array because it make for easy itieration on the nav bar -->
     <?php foreach ($options as $option):?>
-        <a href="<?php echo $option['link'];?>" style="text-decoration: none;">
-            <img src="<?php echo $option['image'];?>" width="288px">
+        <a href="<?php echo $option['link'];?>" style="display: inline-block; text-decoration: none; height: 200px;">
+            <img src="<?php echo $option['image'];?>" width="200px">
         </a>
     <?php endforeach;?>
 </nav>
@@ -27,3 +30,4 @@ require_once(__DIR__."/createHeaders.php");
 </div>
 <!-- Sets the background -->
 <div style="background-color:#389afc; height: 1vh;"></div>
+<?php }?>
