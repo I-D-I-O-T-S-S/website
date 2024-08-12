f4c129688ea69f2d7b4cad04ade97ca41b476bb3
banna12345bob <banna12345bob@outlook.com>
2024-08-12 20:36:13 +1000

    Update main page

--- IDIOTSS/index.php
+++ IDIOTSS/index.php
@@ -6,13 +6,13 @@
     ?>
     <body id="body">
         <?php createNav() ?>
-        <!-- <img src="<?php echo $path?>/images/egg.gif" style="display:inline-block" height=10%> -->
-        <h1  class="title">Web design is my passion</h1>
-        <!-- <img src="<?php echo $path?>/images/egg.gif" style="display:inline-block" height=10%> -->
-        <p class="url"><a href="<?php echo $path?>/dynmap">Dynmap</a></p>
-        <p class="url"><a href="<?php echo $path?>/train-map">Trains</a></p>
-        <h3 class="url download"><a href="<?php echo $path?>/downloads/IDIOTSS Launcher-setup.exe">Download Launcher<a></h3>
-        <p class="url"><a href="<?php echo $path?>/downloads">Downloads</a></p>
+        <div class="content">
+            <h1  class="title">Web design is my passion</h1>
+            <p class="url"><a href="<?php echo $path?>/dynmap">DYNMAP</a></p>
+            <p class="url"><a href="<?php echo $path?>/train-map">TRAINS</a></p>
+            <h3 class="url download"><a href="<?php echo $path?>/downloads/IDIOTSS Launcher-setup.exe">DOWNLOAD LAUNCHER<a></h3>
+            <!-- <p class="url"><a href="<?php echo $path?>/downloads/changelog">CHANGELOG</a></p> -->
+        </div>
         <script type = "text/javascript" src = "scripts/slide.js"></script>
     </body>
 </html>
